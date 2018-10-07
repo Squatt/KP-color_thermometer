8f80e92816dd194090ebd8f4310e0b863ee73d55
LudovicM <ludovic.mandagot@epitech.eu>
2018-10-08 00:02:02 +0200

    first commit

---
+++ README.m
@@ -0,0 +1,1 @@
+# KP-color_thermometer
---
+++ app/src/main/java/com/enterprise/squatt/kp_color_thermometer/MainActivity.java
@@ -0,0 +1,13 @@
+package com.enterprise.squatt.kp_color_thermometer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
