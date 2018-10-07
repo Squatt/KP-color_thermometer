04136d791635ddfa304acd00f90ad1a97a001555
LudovicM <ludovic.mandagot@epitech.eu>
2018-10-08 00:15:46 +0200

    big improvement

--- app/src/main/java/com/enterprise/squatt/kp_color_thermometer/MainActivity.java
+++ app/src/main/java/com/enterprise/squatt/kp_color_thermometer/MainActivity.java
@@ -1,13 +1,81 @@
 package com.enterprise.squatt.kp_color_thermometer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.enterprise.squatt.MESSAGE";
+    public static final String EXTRA_COLOR_LIST = "com.enterprise.squatt.COLOR_LIST";
+
+    private Button room1Button = null;
+    private Button room2Button = null;
+    private Button room3Button = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Get UI button
+        room1Button = findViewById(R.id.buttonRoom1);
+        room2Button = findViewById(R.id.buttonRoom2);
+        room3Button = findViewById(R.id.buttonRoom3);
+
+        room1Button.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v)
+            {
+                // Initialize colors for Room One
+                Intent intent = new Intent(MainActivity.this, RoomScript.class);
+
+                ArrayList<String> colors = new ArrayList<>();
+                colors.add("#F00011");
+                colors.add("#00F110");
+                colors.add("#1000FF");
+
+                intent.putStringArrayListExtra(EXTRA_COLOR_LIST, colors);
+
+                startActivity(intent);
+            }
+        });
+
+        room2Button.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v)
+            {
+                // Initialize colors for Room Two
+                Intent intent = new Intent(MainActivity.this, RoomScript.class);
+
+                ArrayList<String> colors = new ArrayList<>();
+                colors.add("#FF0002");
+                colors.add("#02F002");
+                colors.add("#0022FF");
+
+                intent.putStringArrayListExtra(EXTRA_COLOR_LIST, colors);
+
+                startActivity(intent);
+            }
+        });
+
+        room3Button.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v)
+            {
+                // Initialize colors for Room Three
+                Intent intent = new Intent(MainActivity.this, RoomScript.class);
+
+                ArrayList<String> colors = new ArrayList<>();
+                colors.add("#FF3333");
+                colors.add("#33FF0F");
+                colors.add("#3003FF");
+
+                intent.putStringArrayListExtra(EXTRA_COLOR_LIST, colors);
+
+                startActivity(intent);
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/enterprise/squatt/kp_color_thermometer/RoomScript.java
@@ -0,0 +1,119 @@
+package com.enterprise.squatt.kp_color_thermometer;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * Created by LudovicM on 08/10/2018.
+ */
+
+public class RoomScript extends AppCompatActivity {
+    final String NAMEACTIVITY = "RoomScript Activity";
+
+    private TextView messageOnUI = null;
+    private View viewTemperature = null;
+    private TextView colorsTransitionText = null;
+
+    List<String> shades;
+    int countDownInterval = 100;
+    int millisInFuture = 10000;
+    int[] timer = new int[] { 10000, 15000, 5000 };
+
+    int startColor = 0;
+    int endColor = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.room_script);
+
+        // get parameter from parent
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        ArrayList<String> listColors = intent.getStringArrayListExtra(MainActivity.EXTRA_COLOR_LIST);
+        shades = listColors;
+        intent.getStringArrayListExtra(MainActivity.EXTRA_COLOR_LIST);
+        //shades = Arrays.asList("#FF0000", "#00FF00", "#0000FF");
+
+        // connect front with back
+        viewTemperature = findViewById(R.id.viewTemperature);
+        colorsTransitionText = findViewById(R.id.transitionIndicator);
+        messageOnUI = findViewById(R.id.textView);
+        messageOnUI.setText(message);
+
+        // initialize loop color
+        millisInFuture = chooseTimeTransition();
+        String colorOne = chooseShade(shades);
+        String colorTwo = chooseShade(shades);
+        startColor = Color.parseColor(colorOne);
+        endColor = Color.parseColor(colorTwo);
+        colorsTransitionText.setText("Transition : " + colorOne + " => " + colorTwo);
+        shadesTransition();
+    }
+
+    void shadesTransition() {
+        new CountDownTimer(millisInFuture, countDownInterval) {
+
+            public void onTick(long millisUntilFinished) {
+                messageOnUI.setText("seconds remaining: " + millisUntilFinished / countDownInterval);
+                int diff = (int) millisUntilFinished / countDownInterval;
+                int colorStep = computeTransitionColorStep(diff, startColor, endColor);
+                viewTemperature.setBackgroundColor(colorStep);
+            }
+
+            public void onFinish() {
+                messageOnUI.setText("done!");
+                millisInFuture = chooseTimeTransition();
+                String colorOne = chooseShade(shades);
+                String colorTwo = chooseShade(shades);
+                startColor = Color.parseColor(colorOne);
+                endColor = Color.parseColor(colorTwo);
+                colorsTransitionText.setText("Transition : " + colorOne + " => " + colorTwo);
+                shadesTransition();
+            }
+        }.start();
+    }
+
+    int computeTransitionColorStep(double diff, int currentColor, int colorToReach) {
+        double ratio = diff / countDownInterval;
+
+        int RClose = (colorToReach >> 16) & 0xff;
+        int GClose = (colorToReach >>  8) & 0xff;
+        int BClose = (colorToReach      ) & 0xff;
+
+        int R = (currentColor >> 16) & 0xff;
+        int G = (currentColor >>  8) & 0xff;
+        int B = (currentColor      ) & 0xff;
+
+        int red = (int)Math.abs((ratio * R) + ((1 - ratio) * RClose));
+        int green = (int)Math.abs((ratio * G) + ((1 - ratio) * GClose));
+        int blue = (int)Math.abs((ratio * B) + ((1 - ratio) * BClose));
+
+        return Color.rgb(red, green, blue);
+    }
+
+    int chooseTimeTransition() {
+        Random rand = new Random();
+        int n = rand.nextInt(timer.length);
+        return timer[n];
+    }
+
+    String chooseShade(List<String> shades) {
+        Random rand = new Random();
+        int n = rand.nextInt(shades.size());
+        return shades.get(n);
+    }
+}
